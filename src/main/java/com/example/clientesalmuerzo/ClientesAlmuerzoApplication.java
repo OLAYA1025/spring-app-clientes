b22c4762008d5bda067c3cb5c07f525605acbcba
Jeronimo <jeronimoolaya3@gmail.com>
2025-08-30 18:21:25 -0500

    cambio 8

--- src/main/java/com/example/clientesalmuerzo/ClientesAlmuerzoApplication.java
+++ src/main/java/com/example/clientesalmuerzo/ClientesAlmuerzoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.example")
-@EnableJpaRepositories("com.example.services")
-public class ClientesAlmuerzoApplication {
+@SpringBootApplication
 
+public class ClientesAlmuerzoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientesAlmuerzoApplication.class, args);
     }
